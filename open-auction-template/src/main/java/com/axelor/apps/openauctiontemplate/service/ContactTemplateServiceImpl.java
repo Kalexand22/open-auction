18cb673885716f749c82ead04d685c682f6c2d28
Kalexand22 <karl.alexandersson@it-innovations-bzh.fr>
2022-09-07 16:59:56 +0200

    Création contact depuis un modèle

--- open-auction-template/src/main/java/com/axelor/apps/openauctiontemplate/service/ContactTemplateServiceImpl.java
+++ open-auction-template/src/main/java/com/axelor/apps/openauctiontemplate/service/ContactTemplateServiceImpl.java
@@ -2,10 +2,15 @@ package com.axelor.apps.openauctiontemplate.service;
 
 import com.axelor.apps.base.db.Partner;
 import com.axelor.apps.openauction.db.ContactTemplate;
+import java.lang.invoke.MethodHandles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ContactTemplateServiceImpl implements ContactTemplateService {
+  private final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
   public Partner createContactFromTemplate(ContactTemplate contactTemplate) {
+    log.debug("Creation d'un contact depuis un modèle");
     return null;
   }
 }
