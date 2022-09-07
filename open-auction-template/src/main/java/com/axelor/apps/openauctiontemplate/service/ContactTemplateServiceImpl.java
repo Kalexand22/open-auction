7a3ad0d1be592199d5d8dbdc7d0eb238ebb0b143
Kalexand22 <karl.alexandersson@it-innovations-bzh.fr>
2022-09-07 14:19:07 +0200

    Java implements

--- open-auction-template/src/main/java/com/axelor/apps/openauctiontemplate/service/ContactTemplateServiceImpl.java
+++ open-auction-template/src/main/java/com/axelor/apps/openauctiontemplate/service/ContactTemplateServiceImpl.java
@@ -3,7 +3,7 @@ package com.axelor.apps.openauctiontemplate.service;
 import com.axelor.apps.base.db.Partner;
 import com.axelor.apps.openauction.db.ContactTemplate;
 
-public class ContactTemplateServiceImpl {
+public class ContactTemplateServiceImpl implements ContactTemplateService {
 
   public Partner createContactFromTemplate(ContactTemplate contactTemplate) {
     return null;
