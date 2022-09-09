7532952805132df6bf97ade3962b0e6e3b6c08e9
Kalexand22 <karl.alexandersson@it-innovations-bzh.fr>
2022-09-09 09:35:49 +0200

    OK

--- open-auction-template/src/main/java/com/axelor/apps/openauctiontemplate/controller/ContactTemplateController.java
+++ open-auction-template/src/main/java/com/axelor/apps/openauctiontemplate/controller/ContactTemplateController.java
@@ -1,6 +1,7 @@
 package com.axelor.apps.openauctiontemplate.controller;
 
 import com.axelor.apps.openauction.db.ContactTemplate;
+import com.axelor.apps.base.db.Partner;
 import com.axelor.apps.openauction.db.repo.ContactTemplateRepository;
 import com.axelor.apps.openauctiontemplate.service.ContactTemplateService;
 import com.axelor.inject.Beans;
@@ -19,6 +20,19 @@ public class ContactTemplateController {
     ContactTemplate contactTemplate =
         Beans.get(ContactTemplateRepository.class)
             .find(Long.parseLong(contactTemplateMap.get("id").toString()));
+    
+    Partner tmpPartner = new Partner(); 
+    tmpPartner.setName( (String) context.get("name"));
+    tmpPartner.setMainAddress( (HashMap<String, Object>) context.get("mainAddress"));
+    tmpPartner.setFixedPhone( (String) context.get("fixedPhone"));
+    tmpPartner.setMobilePhone( (String) context.get("mobilePhone"));
+    tmpPartner.setFax( (String) context.get("fax"));
+   /* EmailAdress emailAddress = new EmailAdress();
+    emailAddress.setEmailAddress( (String) context.get("emailAddress"));
+    tmpPartner.setEmailAddress(emailAddress); 
+   */
+    tmpPartner.setTitleSelect( (Integer) context.get("titleSelect"));
+    tmpPartner.setFirstName((String) context.get("firstName"));
 
     ContactTemplateService contactTemplateService = Beans.get(ContactTemplateService.class);
     contactTemplateService.createContactFromTemplate(contactTemplate);
